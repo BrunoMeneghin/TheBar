4fcf49da236200591ef5a837bd5c956619cace18
Bruno Meneghin <brunomeneghin@icloud.com>
2021-03-04 01:21:49 -0300

    Decorating layout

---
+++ TheBar/View/Layout/DecorateShowProductsBeerLayout.swift
@@ -0,0 +1,8 @@
+//
+//  DecorateShowProductsBeerLayout.swift
+//  TheBar
+//
+//  Created by Bruno Meneghin on 03/03/21.
+//
+
+import Foundation
---
+++ TheBar/View/Layout/Drawable.swift
@@ -0,0 +1,8 @@
+//
+//  Drawable.swift
+//  TheBar
+//
+//  Created by Bruno Meneghin on 04/03/21.
+//
+
+import Foundation
